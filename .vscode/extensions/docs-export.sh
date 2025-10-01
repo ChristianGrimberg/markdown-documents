d0cba93b00cc9cfa44c0b3f431cc0e93b4c75f41
Christian Grimberg <christian.grimberg@outlook.com>
2025-10-01 14:21:10 +0000

    :wrench: Se cambian las tareas de generacion de documentos para EL_LUGAR e interloked-doors.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- .vscode/extensions/docs-export.sh
+++ .vscode/extensions/docs-export.sh
@@ -1,43 +1,19 @@
 #/bin/bash
 case $1 in
-  "-gamp5")
-		pandoc docs/ISPE_GAMP_5/*.md --metadata-file=config/meta-pdf.yml -o ISPE_GAMP_5.pdf
-		echo "Finish export ISPE GAMP 5 Computerized Systems to PDF"
-		pandoc docs/ISPE_GAMP_5/*.md --epub-cover-image=docs/ISPE_GAMP_5/img/portrait-logo.png -o ISPE_GAMP_5.epub -t epub2
-		echo "Finish export ISPE GAMP 5 Computerized Systems to ePub"
-		pandoc docs/ISPE_GAMP_5/*.md -o ISPE_GAMP_5.docx
-		echo "Finish export ISPE GAMP 5 Computerized Systems to Word"
+  "-el_lugar")
+		pandoc docs/EL_LUGAR/*.md --metadata-file=config/meta-pdf.yml -o EL_LUGAR.pdf
+		echo "Finish export EL LUGAR to PDF"
+		pandoc docs/EL_LUGAR/*.md --epub-cover-image=docs/EL_LUGAR/img/portrait-logo.png -o EL_LUGAR.epub -t epub2
+		echo "Finish export EL LUGAR to ePub"
+		pandoc docs/EL_LUGAR/*.md -o EL_LUGAR.docx
+		echo "Finish export EL LUGAR to Word"
 	;;
-	"-infra")
-		pandoc docs/ISPE_GAMP_Infrastructure/*.md --metadata-file=config/meta-pdf.yml -o ISPE_GAMP_Infrastructure.pdf
-		echo "Finish export ISPE GAMP Infrastructure IT to PDF"
-		pandoc docs/ISPE_GAMP_Infrastructure/*.md -o ISPE_GAMP_Infrastructure.epub -t epub2
-		echo "Finish export ISPE GAMP Infrastructure IT to ePub"
-		pandoc docs/ISPE_GAMP_Infrastructure/*.md -o ISPE_GAMP_Infrastructure.docx
-		echo "Finish export ISPE GAMP Infrastructure IT to Word"
-	;;
-	"-lab")
-		pandoc docs/ISPE_GAMP_Laboratory/*.md --metadata-file=config/meta-pdf.yml -o ISPE_GAMP_Laboratory.pdf
-		echo "Finish export ISPE GAMP Laboratory Systems to PDF"
-		pandoc docs/ISPE_GAMP_Laboratory/*.md -o ISPE_GAMP_Laboratory.epub -t epub2
-		echo "Finish export ISPE GAMP Laboratory Systems to ePub"
-		pandoc docs/ISPE_GAMP_Laboratory/*.md -o ISPE_GAMP_Laboratory.docx
-		echo "Finish export ISPE GAMP Laboratory Systems to Word"
-	;;
-	"-data")
-		pandoc docs/ISPE_GAMP_DataIntegrity/*.md --metadata-file=config/meta-pdf.yml -o ISPE_GAMP_DataIntegrity.pdf
-		echo "Finish export ISPE GAMP Records and Data Integrity to PDF"
-		pandoc docs/ISPE_GAMP_DataIntegrity/*.md -o ISPE_GAMP_DataIntegrity.epub -t epub2
-		echo "Finish export ISPE GAMP Records and Data Integrity to ePub"
-		pandoc docs/ISPE_GAMP_DataIntegrity/*.md -o ISPE_GAMP_DataIntegrity.docx
-		echo "Finish export ISPE GAMP Records and Data Integrity to Word"
-	;;
-	"-lib4317")
-		pandoc docs/FDA_LIB_4317/*.md --metadata-file=config/meta-pdf.yml -o FDA_LIB_4317.pdf
-		echo "Finish export FDA LIB 4317 to PDF"
-		pandoc docs/FDA_LIB_4317/*.md -o FDA_LIB_4317.epub -t epub2
-		echo "Finish export FDA LIB 4317 to to ePub"
-		pandoc docs/FDA_LIB_4317/*.md -o FDA_LIB_4317.docx
-		echo "Finish export FDA LIB 4317 to to Word"
+	"-interlocked-doors")
+		pandoc docs/interlocked-doors/*.md --metadata-file=config/meta-pdf.yml -o Interlocked-Doors.pdf
+		echo "Finish export Interlocked Doors to PDF"
+		pandoc docs/interlocked-doors/*.md -o Interlocked-Doors.epub -t epub2
+		echo "Finish export Interlocked Doors to ePub"
+		pandoc docs/interlocked-doors/*.md -o Interlocked-Doors.docx
+		echo "Finish export Interlocked Doors to Word"
 	;;
 esac
